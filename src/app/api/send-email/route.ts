4ef170c953e1aab4b7d98790240ed704e4d46fa7
Huzaif <huzaifnaeem29@gmail.com>
2025-02-06 23:32:51 +0500

    fixed some thing

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_XrpLoE5q_9jPFPZKV2VEFwkuJEmhZzMNo");
 
 export async function POST(req: Request) {
   try {
